a3501b837ff229e5ff66c61269cd9facdfe45e97
SirSmooth <hatrick.cortes@gmail.com>
2022-05-29 16:40:39 -0400

    p_gross listo

--- codigo.py
+++ codigo.py
@@ -6,10 +6,7 @@ np.random.seed(42)
 
 # Definimos parametros para el MC
 M_i = 6000  # Tones/hora
-E_i = 24*np.sort(np.random.rand(100)*11111111111)  # kWh
-eta1 = np.linspace(0.83, 0.79, 330)*0.007 # Porcentaje de recuperacion mineral
-eta2 = np.linspace(0.71, 0.67, 308)*0.007 # Porcentaje de recuperacion mineral
-eta3 = np.linspace(0.84, 0.8, 334)*0.007 # Porcentaje de recuperacion mineral
+E_i = 24*np.sort(np.random.rand(4*30)*745.7*400*6)  # kWh
 T_avg = 3.5*30  # Dias (promedio entre 3 y 4 meses)
 T_cycle = np.sort(np.random.randint(3*30, 4*30, 100))  # Arreglo lineal entre 3 y 4 meses
 
@@ -18,7 +15,6 @@ ley = 0.007
 r_i = np.sort(np.random.normal(0.8, 0.01, len(T_cycle))) # Recuperacion inicial de material por eficiencia de la maquina
 r_f = np.sort(np.random.normal(0.7, 0.01, len(T_cycle)))  # Recuperacion final de material por eficiencia de la maquina
 eta = np.zeros(((len(T_cycle), (T_cycle[len(T_cycle)-1]))))
-
 
 k = 0
 for i in range(len(T_cycle)*(T_cycle[len(T_cycle)-1])):
@@ -34,54 +30,49 @@ for i in range(len(T_cycle)*(T_cycle[len(T_cycle)-1])):
         xrx = np.linspace(r_i[k], r_f[k], T_cycle[len(T_cycle)-1])*ley  # Usamos el dato maximo para la iteracion
         eta[k, i-T_cycle[len(T_cycle)-1]*k-1] = xrx[i-T_cycle[len(T_cycle)-1]*k-1]
         
-
-'''
 T_rep = 3  # Dias
 C_energy = 0.183  # $/kWh
 omega = 10000  # $/tone
 C_rep = 80000 + 400000  # Costo de la mano de obra + costo de los mill liners
 
 # Inventados
-rho = np.array(np.random.normal(8.95, 0.2, 3))  # Densidad g/cm³
+rho = np.sort(np.random.normal(8.95, 0.2, len(T_cycle)))  # Densidad g/cm³
 
 # Calculados
 rho_avg = np.average(rho)  # g/cm³
+print('--------------------------------------------------------------------')
 print('rho_avg: ', rho_avg)
+print('--------------------------------------------------------------------')
 
-N = np.zeros(3)
-for k in range(0, len(T_cycle)):
-    N[k] = (T_cycle[k] + T_rep)/(T_avg + T_rep)
-    k += 1
+N = np.zeros(len(T_cycle))
+for i in range(len(N)):
+    N[i] = (T_cycle[i] + T_rep)/(T_avg + T_rep)
+    i += 1
 print('N: ', N)
+print('--------------------------------------------------------------------')
 
 C_DT = omega*M_i
 print('C_DT: ', C_DT)
+print('--------------------------------------------------------------------')
 
-C_ins = 20000/11*np.linspace(0.33, 0.825, 350) # Inspeccion mensual
-print('C_ins: [', C_ins[0]*30,', ', C_ins[349]*30,']')
+C_ins = 20000/11*np.linspace(0.33, 0.825, 4*30) # Inspeccion mensual
+print('C_ins: [', C_ins[0]*30,', ', C_ins[4*30-1]*30,']')
+print('--------------------------------------------------------------------')
 
-P_gross = np.zeros(3)
-P_gross1 = 0
-P_gross2 = 0
-P_gross3 = 0
-for i in range(1, T_cycle[0]):
-    P_gross1 = (P_gross1 + M_i*24*eta1[i]*omega - E_i[i]*C_energy - C_ins[i])
-    i += 1
-P_gross1 = (P_gross1 - C_DT - C_rep)*N[0]
-for i in range(1, T_cycle[1]):
-    P_gross2 = (P_gross1 + M_i*24*eta2[i]*omega - E_i[i]*C_energy - C_ins[i])
-    i += 1
-P_gross2 = (P_gross2 - C_DT - C_rep)*N[1]
-for i in range(1, T_cycle[2]):
-    P_gross3 = (P_gross1 + M_i*24*eta3[i]*omega - E_i[i]*C_energy - C_ins[i])
-    i += 1
-P_gross3 = (P_gross3 - C_DT - C_rep)*N[2]
+P_gross = np.zeros(len(T_cycle))
 
-P_gross[0] = P_gross1
-P_gross[1] = P_gross2
-P_gross[2] = P_gross3
+k = 0
+while k <= 99:
+    p_gross = 0
+    for i in range(1, T_cycle[k]-1):
+        p_gross = (p_gross + M_i*24*eta[k, i]*omega - E_i[i]*C_energy - C_ins[i])
+        i += 1
+    P_gross[k] = (p_gross - C_DT - C_rep)*N[k]
+    k += 1
+
 print('P_gross :', P_gross)
-
+print('--------------------------------------------------------------------')
+'''
 P = np.zeros(3)
 for i in range(0, 3):
     P[i] = M_i*24*0.007*0.81*T_cycle[i]*omega
